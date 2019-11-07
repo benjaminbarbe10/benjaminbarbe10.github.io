7f6a0b1f9b207851dc3c896351bcf445eb7de167
Benjamin Barbé <benjaminbarbe10>
2019-11-07 15:23:53 +0100

    Builded repo

---
+++ precache-manifest.097e8fcc88446ff13170b3937ca5a504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532dcc984b2e2355d2008878d8d1f7ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27741ce6be7fb55285be",
+    "url": "/static/css/main.24a35203.chunk.css"
+  },
+  {
+    "revision": "3df6434cc9187ef6b0e0",
+    "url": "/static/js/2.8cef4faf.chunk.js"
+  },
+  {
+    "revision": "27741ce6be7fb55285be",
+    "url": "/static/js/main.fca6b8ad.chunk.js"
+  },
+  {
+    "revision": "69e84f648f72c246c73a",
+    "url": "/static/js/runtime~main.f5520b92.js"
+  }
+]);
