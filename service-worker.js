15533df02b24b5d8874a9b8d5ffb64823caa86ca
System Administrator <benjaminbarbe10>
2019-11-07 16:05:55 +0100

    11/07/19 16:05:55

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3071ced08d9a3184ee78dd4595c96d91.js"
+  "/precache-manifest.c49b3c5510806236d6effc035d43057a.js"
 );
 
 self.addEventListener('message', (event) => {
